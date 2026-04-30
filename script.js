4c6d7b9bb6f648430b180578cce1e4e87b9da0fe
Vincent <vmgboyles@gmail.com>
2026-05-01 01:21:23 +0800

    Got the scoring logic working and score DOM too

--- script.js
+++ script.js
@@ -26,16 +26,22 @@ const getPlayerChoice = () => {
     rock.addEventListener('click', () => {
         playerChoice = '🪨';
         getComputerChoice()
+        winOrLose(playerChoice, computerChoice)
+        changeScore(playerScore, computerScore)
         announceChoice(playerChoice, computerChoice)
     })
     paper.addEventListener('click', () => {
         playerChoice = '📄';
         getComputerChoice()
+        winOrLose(playerChoice, computerChoice)
+        changeScore(playerScore, computerScore)
         announceChoice(playerChoice, computerChoice)
     })
     scissors.addEventListener('click', () => {
         playerChoice = '✂️';
         getComputerChoice()
+        winOrLose(playerChoice, computerChoice)
+        changeScore(playerScore, computerScore)
         announceChoice(playerChoice, computerChoice)
     })
 }
@@ -43,18 +49,13 @@ const getPlayerChoice = () => {
 
 // ANNOUNCE CHOICE & SCORE FUNCTION
 const announceChoice = (player, computer) => {
-    display = `Player choice is ${player} vs Computer choice ${computer}`
-    changeDisplay(display);
-}
-
-const announceScore = (player, computer)  => {
-    display = `[Player Score: ${player}][Computer Score: ${computer}]`
+    display = `😀 choice is ${player} vs 🤖 choice ${computer}`
     changeDisplay(display);
 }
 
 const announceWinner = (player, computer) => {
     if (player > computer) {
-        display = 'Player final score is the highest! PLAYER WINS!'
+        display = '😀 final score is the highest! PLAYER WINS!'
     } else if (player < computer) {
         display = 'Computer final score is the highest! COMPUTER WINS!'
     } else {
@@ -65,9 +66,9 @@ const announceWinner = (player, computer) => {
 // WIN LOSE FUNCTIONS
 
 const winOrLose = (player, computer) => {
-    if ((player == 'ROCK' && computer == 'SCISSORS') ||
-        (player == 'PAPER' && computer == 'ROCK') ||
-        (player == 'SCISSORS' && computer == 'PAPER'))
+    if ((player == '🪨' && computer == '✂️') ||
+        (player == '📄' && computer == '🪨') ||
+        (player == '✂️' && computer == '📄'))
         playerScore++
     else if (player == computer) {
         playerScore;
@@ -83,6 +84,14 @@ const winOrLose = (player, computer) => {
 
 const container = document.querySelector('#display-container')
 const content = document.createElement('p');
+const playerScoreDOM = document.querySelector('#human-score');
+const computerScoreDOM = document.querySelector('#computer-score');
+
+const changeScore = (human, computer) => {
+    playerScoreDOM.textContent = human;
+    computerScoreDOM.textContent = computer;
+}
+
 
 const changeDisplay = (display) => {
     content.textContent = display
@@ -90,7 +99,7 @@ const changeDisplay = (display) => {
 }
 
 const ask = () => {
-    confirm('Want to play 5 rounds of Rock Paper Scissors?') ? true : window.close();
+    confirm('Want to play Rock Paper Scissors? First to 5 wins!') ? true : window.close();
 }
 
 playGame()
